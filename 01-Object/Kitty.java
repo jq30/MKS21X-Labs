5c77473af25cec42d174d84636d78850bae9bf4e
Jing Qian <jqian30@stuy.edu>
2021-10-21 10:56:35 -0400

    initial commit

---
+++ 01-Object/Kitty.java
@@ -0,0 +1,23 @@
+public class Kitty {
+  public void Kitty(String name, int age) {
+
+  }
+
+  public static int getAge() {
+    return 0;
+  }
+
+  public static String getName() {
+    return "";
+  }
+
+  public static void makeOlder() {
+
+  }
+
+  public static void changeName(String newName) {
+
+  }
+
+
+}
