bcdd4016e862132ab4586df01429f81cb12942cf
Jing Qian <jqian30@stuy.edu>
2021-10-22 14:31:38 -0400

    static distance functions

---
+++ 01-Object/Demo.java
@@ -0,0 +1,23 @@
+public class Demo{
+  /* more methods can go here*/
+  public static double distance(Point a, Point b) {
+    double xa = a.getX();
+    double ya = a.getY();
+    double xb = b.getX();
+    double yb = b.getY();
+
+    return Math.sqrt(Math.pow(xa - xb, 2) + Math.pow(ya - yb, 2));
+  }
+
+  public static void main(String[]args){
+    Point P1 = new Point(1,1);
+    Point P2 = new Point(2,2);
+
+    //for these points it should return root 2 (about 1.414)
+    System.out.println(  distance(P1,P2)); //call1
+    System.out.println(  Point.distance(P1,P2)); //call 2
+//    System.out.println(  P1.distanceTo(P2)); //call 3
+    //This one is IMPLIED strongly. You cannot tell by the calling style alone,
+    //but you can make an educated guess.
+  }
+ }
---
+++ 01-Object/Point.java
@@ -0,0 +1,21 @@
+public class Point{
+  private double x,y;
+  public Point(double X, double Y){
+    x=X;
+    y=Y;
+  }
+  public Point(Point p){
+    x= p.x;
+    y= p.y;
+  }
+  public double getX(){
+    return x;
+  }
+  public double getY(){
+    return y;
+  }
+
+  public static double distance(Point a, Point b) {
+    return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+  }
+}
