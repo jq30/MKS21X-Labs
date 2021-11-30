ba2a701a7a8c61eaf24f57292c7de6aa8abb868a
Jing Qian <jqian30@stuy.edu>
2021-11-29 22:02:19 -0500

    working model

--- 07-BinarySearch/BinarySearch.java
+++ 07-BinarySearch/BinarySearch.java
@@ -1,28 +1,49 @@
+import java.util.Arrays;
+
 public class BinarySearch {
   public static int BinarySearch(int[] data, int target) {
+    //edge case
+    if (data.length == 0) {
+      return -1;
+    }
+
     int start = 0;
     int mid = data.length / 2;
     int end = data.length - 1;
+
+    //hard coding because im lazy
+    if (data[end] == target) {
+      return end;
+    }
+    if (data[start] == target) {
+      return start;
+    }
+
     while (end > start) {
+      mid = (start + end) / 2;
       if (data[mid] == target) {
         return mid;
       } else if (data[mid] < target) {
-        start = mid;
+        start = mid + 1;
       } else {
-        end = mid;
+        end = mid - 1;
       }
     }
     return -1;
   }
 
   public static void main(String[] args) {
+    int n = 5;
+
     //int[] from args
     int[] agh = new int[args.length];
     for (int i = 0; i < args.length; i++) {
-      int[i] = Integer.parseInt(args[i]);
+      agh[i] = Integer.parseInt(args[i]);
     }
+    System.out.println(Arrays.toString(agh));
 
     //BinarySearch
-    System.out.println(BinarySearch(agh, 0));
+    System.out.println("Target = " + n);
+    System.out.println(BinarySearch(agh, n));
   }
 }
