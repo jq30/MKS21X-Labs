691c96386c54f5b7cb40e71a6557c1e01fa13ed1
Jing Qian <jqian30@stuy.edu>
2021-12-01 10:29:04 -0500

    one iteration

---
+++ 08-Sorts/Sorts.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class Sorts {
+  public static void bubbleSort(int[] data) {
+    for (int i = 0; i < data.length - 1; i++) {
+      if (data[i] > data[i+1]) {
+        //place to hold the thingy
+        int n = data[i+1];
+        //swap terms
+        data[i+1] = data[i];
+        data[i] = n;
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    int[] a = new int[args.length];
+    for (int i = 0; i < args.length; i++) {
+      a[i] = Integer.parseInt(args[i]);
+    }
+
+    System.out.println("Init: " + Arrays.toString(a));
+
+    bubbleSort(a);
+    System.out.println("First Iteration: " + Arrays.toString(a));
+  }
+}
