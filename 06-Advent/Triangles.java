34cebfe29a2f6eef3bd96aeb481c85aa2f4a1aff
Jing Qian <jqian30@stuy.edu>
2021-12-03 15:42:14 -0500

    annoying string

--- 06-Advent/Triangles.java
+++ 06-Advent/Triangles.java
@@ -36,7 +36,7 @@ public class Triangles {
             count++;
           }
         }
-        System.out.println("Number of possible triangles: " + count);
+        System.out.println(count);
       } catch (FileNotFoundException bwahahahahah) {
         System.out.println("File " + filename + " not found!!");
       }
