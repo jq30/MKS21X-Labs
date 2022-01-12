df8c1b683829097f5ec491dc12c573305a6d342c
Jing Qian <jqian30@stuy.edu>
2022-01-12 10:56:35 -0500

    moved loop into method for convenience

--- 12-Stuyablo/Draw.java
+++ 12-Stuyablo/Draw.java
@@ -1,5 +1,8 @@
 public class Draw {
   public static void main(String[] args) {
+    //clear
+    Text.clear();
+
     //create an 80 by 30 border of color
     final int height = 80;
     final int width = 30;
@@ -10,7 +13,15 @@ public class Draw {
     int col = 0;
 
     //draw top row
-    for (col = 1; col <= 30; col++) {
+    drawHorizontal(0, 30, color);
+
+    //pl to place cursor on next row
+    System.out.println();
+  }
+
+  private static void drawHorizontal(int row, int length, int color) {
+    int col = 0;
+    for (col = 1; col <= length; col++) {
       Text.go(1, col);
       System.out.print(Text.colorize("M", color));
     }
