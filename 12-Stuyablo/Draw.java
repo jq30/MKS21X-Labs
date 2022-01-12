c7154097ee847207e5834d1bd4efdc97a2e560f6
Jing Qian <jqian30@stuy.edu>
2022-01-12 11:02:12 -0500

    drawVertical

--- 12-Stuyablo/Draw.java
+++ 12-Stuyablo/Draw.java
@@ -15,6 +15,8 @@ public class Draw {
     //draw border
     drawHorizontal(0, 30, color); //top row
     drawHorizontal(height, 30, color); //bottom row
+    drawVertical(0, 30, color); //left col
+    drawVertical(width, 30, color); //right col
 
     //pl to place cursor on next row
     System.out.println();
@@ -23,6 +25,14 @@ public class Draw {
   private static void drawHorizontal(int row, int length, int color) {
     int col = 0;
     for (col = 1; col <= length; col++) {
+      Text.go(row, col);
+      System.out.print(Text.colorize("M", color));
+    }
+  }
+
+  private static void drawVertical(int col, int length, int color) {
+    int row = 0;
+    for (row = 1; row <= length; row++) {
       Text.go(row, col);
       System.out.print(Text.colorize("M", color));
     }
