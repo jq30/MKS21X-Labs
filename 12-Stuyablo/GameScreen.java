baf7427e1303dc4e5a08e7a248788fe7664a201e
Jing Qian <jqian30@stuy.edu>
2022-01-12 15:59:01 -0500

    cleaned up by moving things from main into methods

--- 12-Stuyablo/GameScreen.java
+++ 12-Stuyablo/GameScreen.java
@@ -1,22 +1,16 @@
-public class Draw {
+public class GameScreen {
   public static void main(String[] args) {
     //clear
     Text.clear();
 
     //create an 30 by 80 border of color
-    final int height = 30;
     final int width = 80;
+    final int height = 30;
     final int color = Text.BLUE + Text.BACKGROUND;
 
     //cursor position
     int row = 0;
     int col = 0;
-
-    //draw border
-    drawHorizontal(0, width, color); //top row
-    drawHorizontal(height, width, color); //bottom row
-    drawVertical(0, height, color); //left col
-    drawVertical(width, height, color); //right col
 
     //make 4 random ints
     int[] ints = new int[4];
@@ -24,19 +18,28 @@ public class Draw {
       ints[i] = randInt(0, 99);
     }
 
-    //evenly distribute these ints
-    for (int i = 0; i < ints.length; i++) {
-      int increment = (width / ints.length); //spacing to evenly distribute
-      int offset = -(increment / 2); //offset to make it center-aligned
-      Text.go(2, (i + 1) * increment + offset); //magic number 2 comes from it being row after border
-      System.out.print(ints[i]);
-    }
-
-    //place cursor at proper position
-    Text.go(height, 0);
+    drawBorder(width, height, color);
+    distribute(ints, width, height, 2); //2 is row after border
 
     //pl to place cursor on next row
     System.out.println();
+  }
+
+  private static void drawBorder(int width, int height, int color) {
+    drawHorizontal(0, width, color); //top row
+    drawHorizontal(height, width, color); //bottom row
+    drawVertical(0, height, color); //left col
+    drawVertical(width, height, color); //right col
+  }
+
+  private static void distribute(int[] ints, int width, int height, int y) {
+    for (int i = 0; i < ints.length; i++) {
+      int increment = (width / ints.length); //spacing to evenly distribute
+      int offset = -(increment / 2); //offset to make it center-aligned
+      Text.go(y, (i + 1) * increment + offset); //y := y position to print at
+      System.out.print(ints[i]);
+    }
+    Text.go(height, 0); //place cursor at proper position
   }
 
   //return random number from lower to upper inclusive
