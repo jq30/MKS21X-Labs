ea9ea147d4b901b0e5176404d41eca5e33dec0e7
Jing Qian <jqian30@stuy.edu>
2022-01-14 11:02:17 -0500

    changed Wizard class to reflect changes in abstract class adventurer

--- 12-Stuyablo/Wizard.java
+++ 12-Stuyablo/Wizard.java
@@ -1,39 +1,44 @@
 public class Wizard/* (ACTUALLY A MAGE) */ extends Adventurer {
   private int mana;
+  private String flavor;
 
-  public Wizard(String Name, String aaaa, int mana) {
+  public Wizard(String Name, String flavor, int mana) {
     super(Name);
     this.mana = mana;
+    this.flavor = flavor;
   }
 
   //does damage scaling with half of current mana
   //restores mana inversely scaling with amount of damage dealt
-  public void attack(Damageable other) {
+  public String attack(Damageable other) {
     int damage = mana / 2;
     int manaRestore = 30 / (damage + 1) + 1;
 
     other.applyDamage(damage);
     mana += manaRestore;
 
-    System.out.println(this.toString() + " attacked " + other.toString() + " for " + damage + " damage!");
-    System.out.println(this.toString() + " recovered " + manaRestore + " mana!");
+    return this.toString() + " attacked " + other.toString() + " for " + damage + " damage and recovered " + manaRestore + " mana!";
   }
 
   //counsumes all mana and deals damage according to amount of mana consumed
-  public void specialAttack(Damageable other) {
+  public String specialAttack(Damageable other) {
     if (mana > 20) {
       int damage = mana;
       mana = 0;
 
       other.applyDamage(damage);
-      System.out.println(this + " used special attack on " + other + " dealing " + damage + " damage!");
+      return this.toString() + " used special attack on " + other + " dealing " + damage + " damage! " + flavor;
     } else {
-      System.out.println("Not enough mana!");
       attack(other);
+      return("Not enough mana!");
     }
   }
 
   ///////
+
+  public int getmaxHP() {
+    return this.maxHP;
+  }
 
   public int getMana() {
     return mana;
@@ -41,5 +46,13 @@ public class Wizard/* (ACTUALLY A MAGE) */ extends Adventurer {
 
   public void setMana(int n) {
     mana = n;
+  }
+
+  public int getSpecial() {
+    return this.getSpecial();
+  }
+
+  public int getSpecialMax() {
+    return this.getSpecialMax();
   }
 }
